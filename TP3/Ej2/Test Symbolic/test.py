a754cfb902411f9329362c2902686c650ccfd58f
Guido <glambertucci@itba.edu.ar>
2019-09-08 01:27:37 -0300

    Comienzo del punto 2, todas las simulaciones cumplen, falta ajustar valores comerciales. y hacer todo lo analitico del filtro, deberia mejorar mi simbolyc

--- TP3/Ej2/Test Symbolic/test.py
+++ TP3/Ej2/Test Symbolic/test.py
@@ -1,10 +1,18 @@
 import sympy as sp
 
 
-x, y, z = sp.symbols('x, y, z')
+## ingognitas y parámetros
+v1, v2, vx, vout1, vfeed, vout = sp.symbols("v1 v2 vx vout1 vfeed vout")
+r1, r2, r3, r4, r5, r6, r7, r8, r9 = sp.symbols("r1 r2 r3 r4 r5 r6 r7 r8 r9")
 
-eq1 = x + y + z
-eq2 = x + y + 2*z*z
 
-h = sp.solve([eq1-1,eq2-3] , (x,y,z))
-print(h)
+h = sp.solve([
+    (vout1-v1)/r3-(v1-vx)/r4,# = 0
+    (vx-v2)/r6-v2/r7,# = 0
+    (vfeed-vx)/r5-(vx-v2)/r6-(vx-v1)/r4,# = 0
+    vout1/r1+vout/r2# = 0
+    ],
+    (vx, vout1, vfeed, vout)
+)
+print('$V_{Out}',sp.latex((h[vout]).factor()))
+#print(sp.pretty(h[vout]))
