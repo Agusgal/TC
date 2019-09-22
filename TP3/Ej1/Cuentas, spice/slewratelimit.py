b7fc8be1657737b3727a2fca3e844e50eea9ec1c
Alan <31071359+amechu@users.noreply.github.com>
2019-09-22 00:01:56 -0300

    fgjkcvbnm

---
+++ TP3/Ej1/Cuentas, spice/slewratelimit.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = list(range(10**1, 10**8, 100))
+v=[]
+v_sat = 6.75
+for ff in f:
+
+    v_slew=(4.136*10**6)/ff
+
+    if(v_slew < v_sat):
+        v.append(v_slew)
+    else:
+        v.append(v_sat)
+
+plt.title("Tensión de Entrada Máxima del Circuito")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Vin Max [V]")
+plt.grid()
+plt.xscale("log")
+plt.plot(f, v, color='orange')
+plt.show()
+
