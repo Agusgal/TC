f97fc96de11f8e1a5d8b91ab3a1c6ed29dda2082
MT2321 <mrodriguez@itba.edu.ar>
2019-10-19 18:00:02 -0300

    a interval is now inclusive

--- TP4/utils.py
+++ TP4/utils.py
@@ -1,7 +1,7 @@
 def maprange(a, b, s):
   """
     Linearly map a value 's' in a given range [a1, a2] to a value 't' in a range [b1, b2]
-    
+
     Code based on https://rosettacode.org/wiki/Map_range
 
     Parameters
@@ -31,7 +31,7 @@ def maprange(a, b, s):
     """
   (a1, a2), (b1, b2) = a, b
   #s must be in the interval [a1, a2]
-  if a1 < s < a2:
+  if a1 <= s <= a2:
     return  b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
   else:
     raise ValueError(f'Value {s} is out of bounds')
